9b704986661216ab9c8ae9c5528fef57b4d821a9
Cody Mithell <35934321+codymitchell22@users.noreply.github.com>
2018-04-30 15:38:03 -0500

    Delete temp_mine.py

--- temp_mine.py
+++
@@ -1,8 +0,0 @@
-from temp_header import temp_sensor
-import time
-tempsensor = temp_sensor()
-
-while True:
-	temp = tempsensor.readTempF()
-	print ("Temperature in Fahrenheit : %.2f F"%(temp['f']))
-	time.sleep(1)
